0deebba7954469c9cb667f4ed67f270b5cd5032a
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-01 13:15:09 +0545

    feat(serviceWorker): Reacting to Incoming Events (in SW)

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  console.log(
+    `[Service Worker] Installing Service Worker ... ${JSON.stringify(event)}`
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log(
+    `[Service Worker] Activating Service Worker ... ${JSON.stringify(event)}`
+  );
+});
