846a2592e650e8462fec8d059c0e860b3e5fa7c3
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-01 13:21:33 +0545

    feat(serviceWorker): Non-Lifecycle Events

--- public/sw.js
+++ public/sw.js
@@ -9,3 +9,10 @@ self.addEventListener('activate', event => {
     `[Service Worker] Activating Service Worker ... ${JSON.stringify(event)}`
   );
 });
+
+self.addEventListener('fetch', event => {
+  console.log(
+    `[Service Workder] Fetching something ... ${JSON.stringify(event)}`
+  );
+  event.respondWith(fetch(event.request));
+});
