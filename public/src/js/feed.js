1d995279b267ef22957a2e7d29781165c1af0b08
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-01 14:16:42 +0545

    feat(serviceWorker): Deferring the App Install Banner

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,21 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(choiceResult => {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
+
+    deferredPrompt = null;
+  }
 }
 
 function closeCreatePostModal() {
