dda47fe987d540f0b0e19afc4d31640c62c09a31
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-01 17:04:37 +0545

    feat(caching): Cache Multiple Files with addAll

--- public/sw.js
+++ public/sw.js
@@ -5,9 +5,21 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('static').then(cache => {
       console.log('[Servie Worker] Precaching App Shell');
-      cache.add('/');
-      cache.add('/index.html');
-      cache.add('/src/js/app.js');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ]);
     })
   );
 });
