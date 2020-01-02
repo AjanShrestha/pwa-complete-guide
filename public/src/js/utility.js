59f3ae4c51ead4be28ab3ea4d3f7a94f0f2fc49b
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-02 16:11:38 +0545

    feat(indexdbDynamicData): Using IndexedDB in the Service Worker

---
+++ public/src/js/utility.js
@@ -0,0 +1,14 @@
+const dbPromise = idb.open('posts-store', 1, db => {
+  if (!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', {keyPath: 'id'});
+  }
+});
+
+function writeData(st, data) {
+  return dbPromise.then(db => {
+    const tx = db.transaction(st, 'readwrite');
+    const store = tx.objectStore(st);
+    store.put(data);
+    return tx.complete;
+  });
+}
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 importScripts('/src/js/idb.js');
+importScripts('/src/js/utility.js');
 
 const CACHE_STATIC_NAME = 'static-v9';
 const CACHE_DYNAMIC_NAME = 'dynamic-v3';
@@ -19,12 +20,6 @@ const STATIC_FILES = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
 ];
-
-const dbPromise = idb.open('posts-store', 1, db => {
-  if (!db.objectStoreNames.contains('posts')) {
-    db.createObjectStore('posts', {keyPath: 'id'});
-  }
-});
 
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName).then(cache => {
@@ -85,12 +80,7 @@ self.addEventListener('fetch', event => {
         const clonedRes = res.clone();
         clonedRes.json().then(data => {
           for (let key in data) {
-            dbPromise.then(db => {
-              const tx = db.transaction('posts', 'readwrite');
-              const store = tx.objectStore('posts');
-              store.put(data[key]);
-              return tx.complete;
-            });
+            writeData('posts', data[key]);
           }
         });
         return res;
