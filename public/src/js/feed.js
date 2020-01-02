347516c523d30ea82d6a3bc1f34e50675e71f84d
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-02 19:59:33 +0545

    feat(responsiveUI): Adding Animations

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -6,7 +6,9 @@ var closeCreatePostModalButton = document.querySelector(
 var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  setTimeout(() => {
+    createPostArea.style.transform = 'translateY(0)';
+  }, 1);
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
@@ -33,7 +35,7 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.transform = 'translateY(100vh)';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
