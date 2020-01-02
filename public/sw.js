9061d54f85eaf0a98fcd955b964bd2282afba831
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-02 12:37:31 +0545

    feat(advanceCaching): Applying Cache Only

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,21 @@
 const CACHE_STATIC_NAME = 'static-v7';
 const CACHE_DYNAMIC_NAME = 'dynamic-v3';
+const STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+];
 
 self.addEventListener('install', event => {
   console.log(
@@ -8,22 +24,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME).then(cache => {
       console.log('[Servie Worker] Precaching App Shell');
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/offline.html',
-        '/src/js/app.js',
-        '/src/js/feed.js',
-        '/src/js/promise.js',
-        '/src/js/fetch.js',
-        '/src/js/material.min.js',
-        '/src/css/app.css',
-        '/src/css/feed.css',
-        '/src/images/main-image.jpg',
-        'https://fonts.googleapis.com/css?family=Roboto:400,700',
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-      ]);
+      cache.addAll(STATIC_FILES);
     })
   );
 });
@@ -59,6 +60,12 @@ self.addEventListener('fetch', event => {
         })
       )
     );
+  } else if (
+    new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(
+      event.request.url
+    )
+  ) {
+    event.respondWith(caches.match(event.request));
   } else {
     event.respondWith(
       caches.match(event.request).then(response => {
