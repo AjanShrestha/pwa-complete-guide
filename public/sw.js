1e5500bc1e22158363e63d783b40fc8be7f8e224
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-04 21:29:59 +0545

    feat(webPushNotifications): Reacting to Notification Interaction - Clicks

--- public/sw.js
+++ public/sw.js
@@ -161,3 +161,17 @@ self.addEventListener('sync', event => {
     );
   }
 });
+
+self.addEventListener('notificationclick', event => {
+  const notification = event.notification;
+  const action = event.action;
+
+  console.log(notification);
+
+  if (action === 'confirm') {
+    console.log('Confirm was chosen');
+  } else {
+    console.log(action);
+  }
+  notification.close();
+});
