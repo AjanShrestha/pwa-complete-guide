05df2627ccd5c620f787549e86422a20fbe58068
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-04 21:12:19 +0545

    feat(webPushNotifications): Understanding Notifications Options

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -24,6 +24,12 @@ function displayConfirmNotificaiton() {
   if ('serviceWorker' in navigator) {
     const options = {
       body: 'You successfully subscribed to our Notification service!',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US', // BCP 47
+      vibrate: [100, 50, 200], // vibration pause vibration ...
+      badge: '/src/images/icons/app-icon-96x96.png',
     };
     navigator.serviceWorker.ready.then(swreg => {
       swreg.showNotification('Successfully subscribed (from sw)', options);
