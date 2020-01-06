b74c719d8d78e4a165066a829b9492805318118d
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-06 11:11:08 +0545

    refactor(nativeDeviceFeatures): Use updated formidable package and fix linting issue

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 const webpush = require('web-push');
-const formidable = require('formidable');
+const formidable = require('formidable-serverless');
 const fs = require('fs');
 const UUID = require('uuid-v4');
 
@@ -24,77 +24,77 @@ exports.storePostData = functions.https.onRequest((request, response) => {
     const uuid = UUID();
     let formData = new formidable.IncomingForm();
     formData.parse(request, (err, fields, files) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      fs.rename(files.file.path, `/tmp/${files.file.name}`);
-      let bucket = storage.bucket('pwagram-e7d99.appspot.com');
-      bucket.upload(
-        `/tmp/${files.file.name}`,
-        {
-          uploadType: 'media',
-          metadata: {
+      if (!err) {
+        fs.rename(files.file.path, `/tmp/${files.file.name}`);
+        let bucket = storage.bucket('pwagram-e7d99.appspot.com');
+        bucket.upload(
+          `/tmp/${files.file.name}`,
+          {
+            uploadType: 'media',
             metadata: {
-              contentType: files.file.type,
-              firebaseStorageDownloadTokens: uuid,
+              metadata: {
+                contentType: files.file.type,
+                firebaseStorageDownloadTokens: uuid,
+              },
             },
           },
-        },
-        (err, file) => {
-          if (!err) {
-            admin
-              .database()
-              .ref('posts')
-              .push({
-                id: fields.id,
-                title: fields.title,
-                location: fields.location,
-                image: `https://firebasestorage.googleapis.com/v0/b/${
-                  bucket.name
-                }/o/${encodeURIComponent(file.name)}?alt=media&token=${uuid}`,
-              })
-              .then(() => {
-                webpush.setVapidDetails(
-                  'mailto:ajan.shresh@gmail.com',
-                  'BPMh9Hdrri8tao7OUshwND1y98BYJmRoxDEcbtZ4MJN3MITRPTo2u7YCRO7PFWbgtqzjpS_uH4vaGIsx1BNEvs8',
-                  functions.config().pwagram.privatekey
-                );
-                return admin
-                  .database()
-                  .ref('subscriptions')
-                  .once('value');
-              })
-              .then(subscriptions => {
-                subscriptions.forEach(sub => {
-                  const pushConfig = {
-                    endpoint: sub.val().endpoint,
-                    keys: {
-                      auth: sub.val().keys.auth,
-                      p256dh: sub.val().keys.p256dh,
-                    },
-                  };
-                  webpush
-                    .sendNotification(
-                      pushConfig,
-                      JSON.stringify({
-                        title: 'New Post',
-                        content: 'New Post added',
-                        openUrl: '/help',
-                      })
-                    )
-                    .catch(err => console.error(err));
-                });
-                return response
-                  .status(201)
-                  .json({message: 'Data stored', id: fields.id});
-              })
-              .catch(err => response.status(500).json({error: err}));
-          } else {
-            console.error(err);
+          (err, file) => {
+            if (!err) {
+              admin
+                .database()
+                .ref('posts')
+                .push({
+                  id: fields.id,
+                  title: fields.title,
+                  location: fields.location,
+                  image: `https://firebasestorage.googleapis.com/v0/b/${
+                    bucket.name
+                  }/o/${encodeURIComponent(file.name)}?alt=media&token=${uuid}`,
+                })
+                .then(() => {
+                  webpush.setVapidDetails(
+                    'mailto:ajan.shresh@gmail.com',
+                    'BPMh9Hdrri8tao7OUshwND1y98BYJmRoxDEcbtZ4MJN3MITRPTo2u7YCRO7PFWbgtqzjpS_uH4vaGIsx1BNEvs8',
+                    functions.config().pwagram.privatekey
+                  );
+                  return admin
+                    .database()
+                    .ref('subscriptions')
+                    .once('value');
+                })
+                .then(subscriptions => {
+                  subscriptions.forEach(sub => {
+                    const pushConfig = {
+                      endpoint: sub.val().endpoint,
+                      keys: {
+                        auth: sub.val().keys.auth,
+                        p256dh: sub.val().keys.p256dh,
+                      },
+                    };
+                    webpush
+                      .sendNotification(
+                        pushConfig,
+                        JSON.stringify({
+                          title: 'New Post',
+                          content: 'New Post added',
+                          openUrl: '/help',
+                        })
+                      )
+                      .catch(err => console.error(err));
+                  });
+                  return response
+                    .status(201)
+                    .json({message: 'Data stored', id: fields.id});
+                })
+                .catch(err => response.status(500).json({error: err}));
+            } else {
+              console.error(err);
+            }
           }
-        }
-      );
+        );
+      } else {
+        console.error(err);
+      }
     });
   });
 });
