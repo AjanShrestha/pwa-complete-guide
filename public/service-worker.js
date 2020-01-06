644aa8c2b112229bce278e2b480ae580b0d7e706
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-06 14:40:19 +0545

    feat(swMgmtWithWorkbox): Configuring Workbox Precaching

--- public/service-worker.js
+++ public/service-worker.js
@@ -34,10 +34,6 @@ self.__precacheManifest = [
     "revision": "2cab47d9e04d664d93c8d91aec59e812"
   },
   {
-    "url": "help/index.html",
-    "revision": "17131fab2d7057230e22cf111aba858f"
-  },
-  {
     "url": "index.html",
     "revision": "2ad3e6c6ac10185df69853aed6892b3e"
   },
@@ -62,88 +58,8 @@ self.__precacheManifest = [
     "revision": "1c6d81b27c9d423bece9869b07a7bd73"
   },
   {
-    "url": "src/images/icons/app-icon-144x144.png",
-    "revision": "83011e228238e66949f0aa0f28f128ef"
-  },
-  {
-    "url": "src/images/icons/app-icon-192x192.png",
-    "revision": "f927cb7f94b4104142dd6e65dcb600c1"
-  },
-  {
-    "url": "src/images/icons/app-icon-256x256.png",
-    "revision": "86c18ed2761e15cd082afb9a86f9093d"
-  },
-  {
-    "url": "src/images/icons/app-icon-384x384.png",
-    "revision": "fbb29bd136322381cc69165fd094ac41"
-  },
-  {
-    "url": "src/images/icons/app-icon-48x48.png",
-    "revision": "45eb5bd6e938c31cb371481b4719eb14"
-  },
-  {
-    "url": "src/images/icons/app-icon-512x512.png",
-    "revision": "d42d62ccce4170072b28e4ae03a8d8d6"
-  },
-  {
-    "url": "src/images/icons/app-icon-96x96.png",
-    "revision": "56420472b13ab9ea107f3b6046b0a824"
-  },
-  {
-    "url": "src/images/icons/apple-icon-114x114.png",
-    "revision": "74061872747d33e4e9f202bdefef8f03"
-  },
-  {
-    "url": "src/images/icons/apple-icon-120x120.png",
-    "revision": "abd1cfb1a51ebe8cddbb9ada65cde578"
-  },
-  {
-    "url": "src/images/icons/apple-icon-144x144.png",
-    "revision": "b4b4f7ced5a981dcd18cb2dc9c2b215a"
-  },
-  {
-    "url": "src/images/icons/apple-icon-152x152.png",
-    "revision": "841f96b69f9f74931d925afb3f64a9c2"
-  },
-  {
-    "url": "src/images/icons/apple-icon-180x180.png",
-    "revision": "2e5e6e6f2685236ab6b0c59b0faebab5"
-  },
-  {
-    "url": "src/images/icons/apple-icon-57x57.png",
-    "revision": "cc93af251fd66d09b099e90bfc0427a8"
-  },
-  {
-    "url": "src/images/icons/apple-icon-60x60.png",
-    "revision": "18b745d372987b94d72febb4d7b3fd70"
-  },
-  {
-    "url": "src/images/icons/apple-icon-72x72.png",
-    "revision": "b650bbe358908a2b217a0087011266b5"
-  },
-  {
-    "url": "src/images/icons/apple-icon-76x76.png",
-    "revision": "bf10706510089815f7bacee1f438291c"
-  },
-  {
-    "url": "src/images/main-image-lg.jpg",
-    "revision": "31b19bffae4ea13ca0f2178ddb639403"
-  },
-  {
-    "url": "src/images/main-image-sm.jpg",
-    "revision": "c6bb733c2f39c60e3c139f814d2d14bb"
-  },
-  {
-    "url": "src/images/main-image.jpg",
-    "revision": "5c66d091b0dc200e8e89e56c589821fb"
-  },
-  {
-    "url": "src/images/sf-boat.jpg",
-    "revision": "0f282d64b0fb306daf12050e812d6a19"
-  },
-  {
     "url": "src/js/app.js",
-    "revision": "dfdc7d9598abf1ad30477600e81b79b1"
+    "revision": "602ae485f259dfb01156559d9b9832df"
   },
   {
     "url": "src/js/feed.js",
@@ -172,6 +88,22 @@ self.__precacheManifest = [
   {
     "url": "sw.js",
     "revision": "bd829b7248df27837ba23ddfb524742f"
+  },
+  {
+    "url": "src/images/main-image-lg.jpg",
+    "revision": "31b19bffae4ea13ca0f2178ddb639403"
+  },
+  {
+    "url": "src/images/main-image-sm.jpg",
+    "revision": "c6bb733c2f39c60e3c139f814d2d14bb"
+  },
+  {
+    "url": "src/images/main-image.jpg",
+    "revision": "5c66d091b0dc200e8e89e56c589821fb"
+  },
+  {
+    "url": "src/images/sf-boat.jpg",
+    "revision": "0f282d64b0fb306daf12050e812d6a19"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{html,ico,json,css,png,jpg,js}"
-  ],
-  "swDest": "public/service-worker.js"
+  globDirectory: 'public/',
+  globPatterns: ['**/*.{html,ico,json,css,js}', 'src/images/*.{jpg,png}'],
+  globIgnores: ['../workbox-config.js', 'help/**'],
+  swDest: 'public/service-worker.js',
 };
