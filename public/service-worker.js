3876027c4509952ea75b5fd99b657ecdad0b287b
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-06 18:41:01 +0545

    feat(swMgmtWithWorkbox): Handling Background Synchronisation and Push Notifications

--- public/service-worker.js
+++ public/service-worker.js
@@ -142,11 +142,7 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "sw-base.js",
-    "revision": "abbe31ee3d2135e0c2c2ed8cbbe9c348"
-  },
-  {
-    "url": "sw.js",
-    "revision": "bd829b7248df27837ba23ddfb524742f"
+    "revision": "bd3bfea42cb6f7af3b8f185cc7dadd27"
   },
   {
     "url": "src/images/main-image-lg.jpg",
@@ -165,3 +161,93 @@ workbox.precaching.precacheAndRoute([
     "revision": "0f282d64b0fb306daf12050e812d6a19"
   }
 ]);
+
+self.addEventListener('sync', event => {
+  const url =
+    'https://us-central1-pwagram-e7d99.cloudfunctions.net/storePostData';
+  console.log(`[Service Worker] Background syncing ${JSON.stringify(event)}`);
+  if (event.tag === 'sync-new-posts') {
+    console.log('[Service Worker] Syncing new Posts');
+    event.waitUntil(
+      readAllData('sync-posts').then(data => {
+        for (let datum of data) {
+          let postData = new FormData();
+          postData.append('id', datum.id);
+          postData.append('title', datum.title);
+          postData.append('location', datum.location);
+          postData.append('rawLocationLat', datum.rawLocation.lat);
+          postData.append('rawLocationLng', datum.rawLocation.lng);
+          postData.append('file', datum.picture, datum.id + '.png');
+          fetch(url, {
+            method: 'POST',
+            body: postData,
+          })
+            .then(res => {
+              console.log(res);
+              console.log('Sent data', JSON.stringify(res));
+              if (res.ok) {
+                res
+                  .json()
+                  .then(resData =>
+                    deleteItemFromData('sync-posts', resData.id)
+                  );
+              }
+            })
+            .catch(err => {
+              console.log('Error while sending data', err);
+            });
+        }
+      })
+    );
+  }
+});
+
+self.addEventListener('notificationclick', event => {
+  const notification = event.notification;
+  const action = event.action;
+
+  console.log(notification);
+
+  if (action === 'confirm') {
+    console.log('Confirm was chosen');
+  } else {
+    console.log(action);
+    event.waitUntil(
+      clients.matchAll().then(clis => {
+        let client = clis.find(c => c.visibility === 'visible');
+
+        if (client !== undefined) {
+          client.navigate(notification.data.url);
+          client.focus();
+        } else {
+          clients.openWindow(notification.data.url);
+        }
+      })
+    );
+  }
+  notification.close();
+});
+
+self.addEventListener('notificationclose', event => {
+  console.log(`Notification was close ${JSON.stringify(event)}`);
+});
+
+self.addEventListener('push', event => {
+  console.log(`Push Notification received ${JSON.stringify(event)}`);
+
+  let data = {title: 'New!', content: 'Fallback', openUrl: '/'};
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+
+  const options = {
+    body: data.content,
+    icon: '/src/images/icons/app-icon-96x96.png',
+    badge: '/src/images/icons/app-icon-96x96.png',
+    data: {
+      url: data.openUrl,
+    },
+  };
+
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
