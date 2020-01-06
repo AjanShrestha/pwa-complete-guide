e96679feaea8ed40b8eccea098cb0eea5f3fcb6f
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-06 15:10:53 +0545

    feat(swMgmtWithWorkbox): Expanding Dynamic Caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,26 @@ importScripts(
 );
 
 workbox.routing.registerRoute(
+  /.*(?:firebasestorage\.googleapis)\.com.*$/,
+  // cache then network
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'post-images',
+  })
+);
+
+workbox.routing.registerRoute(
   /.*(?:googleapis|gstatic)\.com.*$/,
   // cache then network
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'google-fonts',
+  })
+);
+
+workbox.routing.registerRoute(
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  // cache then network
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'material-css',
   })
 );
 
@@ -73,7 +89,7 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "sw-base.js",
-    "revision": "9d92de6522bd058ac21991f0f7b397a9"
+    "revision": "f6763e0886f64f9dfee777639eb0e486"
   },
   {
     "url": "sw.js",
--- public/sw-base.js
+++ public/sw-base.js
@@ -3,10 +3,26 @@ importScripts(
 );
 
 workbox.routing.registerRoute(
+  /.*(?:firebasestorage\.googleapis)\.com.*$/,
+  // cache then network
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'post-images',
+  })
+);
+
+workbox.routing.registerRoute(
   /.*(?:googleapis|gstatic)\.com.*$/,
   // cache then network
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'google-fonts',
+  })
+);
+
+workbox.routing.registerRoute(
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  // cache then network
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'material-css',
   })
 );
 
